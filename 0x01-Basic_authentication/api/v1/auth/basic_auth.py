742ada53be3609b9f5fa893e7d6d6905db53cfc8
Safuan04 <elmai.safuan@gmail.com>
2024-02-13 19:57:15 +0100

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -36,3 +37,18 @@ class BasicAuth(Auth):
         decoded_value = base64.b64decode(base64_authorization_header)
         utf8_decoded_value = decoded_value.decode('utf-8')
         return utf8_decoded_value
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """ Extraction users credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return (email, password)
