7dd71923a8fcaaed18e1b8f7651c17b3d0184af4
Safuan04 <elmai.safuan@gmail.com>
2024-02-13 19:12:00 +0100

    5. Request validation!/6. Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """ Creating the BasicAuth class
 """
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ BasicAuth class
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extracting the base64 authorization header
+        """
