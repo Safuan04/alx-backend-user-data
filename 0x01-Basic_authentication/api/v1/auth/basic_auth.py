36acef290dcece26b85246bb1e7ebf768cae9624
Safuan04 <elmai.safuan@gmail.com>
2024-02-13 20:36:13 +0100

    10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,12 @@
 """ Creating the BasicAuth class
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+
+
+User = TypeVar('User')
 
 
 class BasicAuth(Auth):
@@ -52,3 +56,24 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> User:
+        """ Returns the User instance based on
+        -   his email and password
+        """
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users_list = User.search({'email': user_email})
+        except KeyError:
+            return None
+
+        for user in users_list:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
