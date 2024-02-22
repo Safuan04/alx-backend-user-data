70c25428615d6a790305eed582124ac580ea9845
Safuan04 <elmai.safuan@gmail.com>
2024-02-22 17:40:16 +0100

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Dict
 
 from user import Base, User
 
@@ -31,8 +34,22 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Saves a user to the database"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        """ Save the user to the database
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs: Dict[str, str]):
+        """ This method takes in arbitrary keyword arguments
+        -   and returns the first row found in the users table
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+
         return user
