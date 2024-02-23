bd6b602912fe28367364e3a37d722b5e406048a6
Safuan04 <elmai.safuan@gmail.com>
2024-02-23 11:41:46 +0100

    14. Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,13 +49,13 @@ def logout() -> str:
     """ Logout function
     """
     session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
 
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
+    if user:
         AUTH.destroy_session(user.id)
         redirect(url_for("index"))
-    except Exception:
-        abort(403)
+
+    abort(403)
 
 
 if __name__ == "__main__":
